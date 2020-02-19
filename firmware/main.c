c660c4d266cb1cec64f448a79bc900784ecdac28
Sam Ettinger <ettingersam@gmail.com>
2020-02-19 11:24:25 +0100

    Added some firmware, changed the order of the resistors to minimize ADC errors

--- firmware/main.c
+++ firmware/main.c
@@ -7,15 +7,14 @@ signed int timerCounter = 0;
 #define _BV(bit) (1 << (bit))
 
 void Setup(void) {
-	// Set registers for flicker and setup
-	TCCR0A = (2<<COM0B0) | (1<<WGM01); // Datasheet p100; set PB1 (Pin 3) as PWM pin
+	TCCR0A = (2<<COM0B0) | (2<<WGM00); // Datasheet p100; set PB0 (Pin 1) as PWM pin
 	TCCR0B = (2<<CS00) | (3<<WGM02);   // Datasheet p103; set PWM behavior, set clock scaler to 125 kHz
-	OCR0B = 0;                         // Counter for PWM on PB1 (pin 3)
-	ICR0 = 124;                        // Counter resets every millisecond, good for timing
-	OCR0A = 60;                        // A nice middle-of-the-range value for timing during PWM
-	DDRB = 1<<PORTB1;                  // Set PB1 (pin 3) as output
-	ADMUX = 0b00<<MUX0;                // Set PB0 (pin 1) as analog input
-	ADCSRA = 1<<ADEN | 3<<ADPS0;       // Enable ADC | Set ADC clock to 125 kHz
+	OCR0B = 0;                         // Counter for PWM on PB0 (pin 1)
+	ICR0 = 127;                        // Counter resets approx. every millisecond, good for timing
+	OCR0A = 63;                        // A nice middle-of-the-range value for timing during PWM
+	DDRB = 1<<PORTB0;                  // Datasheet p76; Set PB0 (pin 1) as output
+	ADMUX = 2<<MUX0;                   // Datasheet p131; Set PB2 (pin 4) as analog input
+	ADCSRA = 1<<ADEN | 3<<ADPS0;       // Datasheet p132; Enable ADC | Set ADC clock to 125 kHz
 }
 
 void OCR0A_idle(void) {
@@ -34,22 +33,49 @@ void idle(int cycles) {
 	}
 }
 
-int getEntropy(int bits) {
-	int entropy = 0;
-	for (int bit = 0; bit<bits; bit++) {
-		// Trigger ADC collection
-		ADCSRA = ADCSRA | 1<<ADSC;
-		// Wait for ADC conversion to finish
-		while (ADCSRA & 1<<ADSC) {
-			;
-		}
-		// Write LSB of ADC to entropy byte
-		if (ADCL & 1) {
-			entropy |= _BV(bit);
-		}
-		_delay_us(20);
+
+int getSwitches(void) {
+	int switches = 0;
+	// Trigger ADC collection
+	ADCSRA = ADCSRA | 1<<ADSC;
+	// Wait for ADC conversion to finish
+	while (ADCSRA & 1<<ADSC) {
+		;
 	}
-	return(entropy);
+	if (ADCL > 140) {
+		switches = 0b0000;
+	} else if (ADCL > 106) {
+		switches = 0b0001;
+	} else if (ADCL > 75) {
+		switches = 0b0010;
+	} else if (ADCL > 57) {
+		switches = 0b0011;
+	} else if (ADCL > 47) {
+		switches = 0b0100;
+	} else if (ADCL > 39) {
+		switches = 0b0101;
+	} else if (ADCL > 34) {
+		switches = 0b0110;
+	} else if (ADCL > 30) {
+		switches = 0b0111;
+	} else if (ADCL > 26) {
+		switches = 0b1000;
+	} else if (ADCL > 24) {
+		switches = 0b1001;
+	} else if (ADCL > 22) {
+		switches = 0b1010;
+	} else if (ADCL > 20) {
+		switches = 0b1011;
+	} else if (ADCL > 18) {
+		switches = 0b1100;
+	} else if (ADCL > 17) {
+		switches = 0b1101;
+	} else if (ADCL > 16) {
+		switches = 0b1110;
+	} else {
+		switches = 0b1111;
+	}
+	return(switches);
 }
 
 
@@ -59,15 +85,11 @@ int main(void) {
 	Setup();
 
 	while(1) {
-		// Collect one bit of entropy
-		int rando = getEntropy(1);
-		// If bit is 1, turn motor on; else, turn motor off
-		if (rando & 1) {
-			OCR0B = 125;
-			} else {
-			OCR0B = 0;
-		}
-		// Wait one second
-		idle(1000);
+		// Figure out which switches are switched on
+		int switches = getSwitches();
+		// Set OCR0B 
+		OCR0B = switches<<3;
+		// Wait approx 1/20th of a second before checking again
+		idle(50);
 	}
 }
